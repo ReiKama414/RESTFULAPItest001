88b287ad3f1ca0eb32a0aa8256b0f917ad016e95
Kama <milu77704@gmail.com>
2023-02-18 17:31:42 +0800

    seo test?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    app: {
+      baseURL: '/RESTFULAPItest001/',
+      head: {
+        title: 'RESTFULAPI test 001 | Kama',
+        charset: 'UTF-8',
+        viewport: 'width=device-width, initial-scale=1',
+        meta: [
+          { name: 'description', content: 'RESTFULAPI Test Project.' },
+          { property: 'og:title', content: 'RESTFULAPI test 001 | Kama' },
+          { property: 'og:description', content: 'RESTFULAPI Test Project.' },
+          { property: 'og:locale', content: 'zh-tw' }
+        ],
+      },
+    },
 })
